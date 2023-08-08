e6ea2ff98c5b82d7f634e73bd88e59620d039fae
Kostya <stan.spb@mail.ru>
2023-08-08 19:47:26 +0300

    pysh fix2

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,8 @@
 def calc():
     x = int(input('vvedite'))
     print(x)
-<<<<<<< HEAD
-
     print('пыщ')
-=======
     print('ololo', 'ololo')
->>>>>>> f1b0b7e3a8b6b723c8fe1f6146c19fd4afe58631
+
 
 calc()
